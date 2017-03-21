730c7e721315525bd4bb1284070416f9a0c32a23
alex <aaa123qqq@mail.ru>
2017-03-21 09:44:01 +0300

    post class + ancestors

---
+++ link.rb
@@ -0,0 +1,18 @@
+class Link < Post
+
+  def initialize
+    super
+
+    @url = ""
+  end
+
+  def read_from_console
+    #todo
+  end
+
+  def to_string
+    #todo
+  end
+
+
+end
---
+++ memo.rb
@@ -0,0 +1,14 @@
+class Memo < Post
+
+ #если консьруктор такойже, то можно его не писать, он будет по умолчанию дергаться с родителя
+
+  def read_from_console
+    #todo
+  end
+
+  def to_string
+    #todo
+  end
+
+
+end
